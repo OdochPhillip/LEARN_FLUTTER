7a0d3ead5763079c0342b228b5b4a2de6857d584
OdochPhillip <kosiaodoch@gmail.com>
2024-09-19 08:43:49 -0700

    trying more features on github

--- 9_list.dart
+++ 9_list.dart
@@ -2,4 +2,5 @@ void main(){
   List<String> student = ["a", "b", "c"];
   print("stuent one is called. ${student[0]}");
   print("hello world");// trying committing
+  print("print ony ${student[1]} and ${student[2]}"); //printed only two students
 }
